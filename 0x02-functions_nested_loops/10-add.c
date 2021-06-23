78f0e2e2d20e983cfb315e4b2775baa82cf8b890
Noobyfire <el.fritzswaggz@gmail.com>
2021-06-23 07:41:58 -0700

    sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - calculate 2 numbers
+ *@i: first number to be added
+ *@j: second number to be added
+ * Return: result
+ */
+int add(int i, int j)
+{
+	int sum = i + j;
+
+	return (sum);
+}
