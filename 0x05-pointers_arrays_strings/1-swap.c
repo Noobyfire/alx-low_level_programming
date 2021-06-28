ce63e8effe04bbe726b696d02b26aaf0a86a73d2
Noobyfire <el.fritzswaggz@gmail.com>
2021-06-28 13:08:50 -0700

    Swapping values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swapping 2 int
+ * @a: value to be swapped
+ * @b: value to be swapped
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+int s = *a;
+int *a = *b;
+int *b = s;
+}
